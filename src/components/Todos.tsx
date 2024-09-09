d27916b34a417da7e3205a6e4a6fb92166c3d729
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-09-09 14:51:33 +0200

    feat: add removeTodoHandler function

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -3,10 +3,10 @@ import Todo from "../models/todo";
 import TodoListItem from "./TodoListItem";
 import classes from './Todos.module.css';
 
-const Todos: React.FC<{items: Todo[]}> = (props) => {
+const Todos: React.FC<{items: Todo[], onRemoveTodo: (id: string) => void}> = (props) => {
     return (
         <ul className={classes.todos}>
-            {props.items.map(item => <TodoListItem  key={item.id} text={item.text} />)}
+            {props.items.map(item => <TodoListItem onRemoveTodo={props.onRemoveTodo.bind(null, item.id)}  key={item.id} text={item.text} />)}
         </ul>
     );
 };
